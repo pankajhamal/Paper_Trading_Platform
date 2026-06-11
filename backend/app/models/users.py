b23a77a14b8fd0ccef58d6dd17687ccb23ad55fd
pankajhamal <pankajhamal0@gmail.com>
2026-06-11 16:42:35 +0545

    fix: clean project structure and apply gitignore

--- backend/app/models/users.py
+++ backend/app/models/users.py
@@ -5,6 +5,7 @@ class User(Base):
     __tablename__="users"
 
     id = Column(Integer, primary_key=True)
+    full_name = Column(String)
     email = Column(String, unique=True)
     password = Column(String)
     balance = Column(Float, default=100000)
